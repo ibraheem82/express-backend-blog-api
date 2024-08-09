0fd878ea2802557391ffefbb613f827d84f59201
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-08-09 15:29:28 +0100

    Implement signup controller for user registration
    - Import the User model for creating new users.
    - Define the signup function to handle user registration.
    - Extract name, email, and password from the request body and create a new User instance.
    - Save the new user to the database and return a success response.
    - Handle errors with a try-catch block and pass them to the next middleware.
    - Export the signup function for use in the auth routes.

---
+++ controllers/auth.js
@@ -0,0 +1,16 @@
+const {User} = require("../models")
+const signup = async (req, res, next) => {
+    try {
+        const {name, email, password} = req.body;
+        const newUser = new User({name, email, password, role})
+        await newUser.save();
+
+        res.status(201).json({message: "User registererd successfully"})
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {signup}
+
+
