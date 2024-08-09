5bd550189914f943fba9c455b0666d0f7817c1e9
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-08-09 15:55:00 +0100

    Implemented POST /signup route, delegating to authController.signup for user registration logic

--- routes/auth.js
+++ routes/auth.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const {authController } = require("../controllers/")
 
-router.post('signup', authController.signup)
+router.post('/signup', authController.signup)
 module.exports = router;
