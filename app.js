b014653df8c2a7e0472554a90b22706504bb4632
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-08-09 14:04:13 +0100

    Initialize Express app with MongoDB connection
    - Set up environment variables using dotenv.
    - Import and connect to MongoDB using the custom connectMongodb function.
    - Configure Express to parse JSON with a size limit of 500MB.
    - Add body-parser middleware to handle URL-encoded data with a size limit of 500MB.
    - Export the Express app instance for use in the server setup.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const dotenv = require('dotenv')
-
 dotenv.config()
+const connectMongodb = require("./init/mongodb")
 
 
 const app = express();
+
+// Connect database
+
+connectMongodb();
 
 app.use(express.json({limit: "500mb"}));
 app.use(bodyParser.urlencoded({limit: "500mb", extended : true}));
