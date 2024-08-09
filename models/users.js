d7e7dda967f72c542b30035f5d22ac648bf17b5b
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-08-09 15:40:55 +0100

    Added user schema and model using Mongoose, including fields for name, email, password, and role with validation and timestamps

---
+++ models/users.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+    {
+    name: {type: String, required:true},
+    email: {type: String, required:true, unique:true, trim:true},
+    password: {type: String, required:true, minlength:6},
+    // role: 1 -> super admin, role: 2 -> normal admin, role: 3 normal user
+    role: {type: Number, default:3},
+},
+    {timestamps: true}
+);
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
