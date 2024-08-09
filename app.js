c46b65214f9d42f28fe48f1297e681d234e44eb7
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-08-09 16:30:55 +0100

    Configured Express app with JSON parsing, error handling middleware, and routes

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const dotenv = require('dotenv');
 const morgan = require("morgan")
 dotenv.config()
 const connectMongodb = require("./init/mongodb")
-const { authRoute } = require('./routes/')
+const { authRoute } = require('./routes/');
+const { errorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
 
 // Connect database
 connectMongodb();
 
+// Third part- middlewares
 app.use(express.json({limit: "500mb"}));
 app.use(bodyParser.urlencoded({limit: "500mb", extended : true}));
 app.use(morgan('dev'))
 
 // Routes
 app.use("/api/v1/auth", authRoute)
+
+// Error handling middleware.
+app.use(errorHandler)
 
 module.exports = app;
