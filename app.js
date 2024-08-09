df7535a58610f44fdce5650595b4644949e73312
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-08-09 16:02:41 +0100

    Enabled request logging with Morgan in development mode

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const morgan = require("morgan")
 dotenv.config()
 const connectMongodb = require("./init/mongodb")
 const { authRoute } = require('./routes/')
@@ -12,6 +13,7 @@ connectMongodb();
 
 app.use(express.json({limit: "500mb"}));
 app.use(bodyParser.urlencoded({limit: "500mb", extended : true}));
+app.use(morgan('dev'))
 
 // Routes
 app.use("/api/v1/auth", authRoute)
