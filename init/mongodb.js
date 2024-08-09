aa771922dd25fc2af873616d3d150a587dbf4187
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-08-09 14:03:15 +0100

    Add MongoDB connection logic
    - Implement asynchronous function to connect to MongoDB using Mongoose.
    - Utilize 'connectionUrl' from the configuration keys for the database connection.
    - Log success message upon successful connection and catch errors for debugging.
    - Export the connection function for use in the application.

---
+++ init/mongodb.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const {connectionUrl} = require("../config/kyes")
+
+const connectMongodb = async () => {
+    try {
+        await mongoose.connect(connectionUrl);
+        console.log("Database connection was successful.")
+    } catch (error) {
+        console.log(error.message)
+    }
+};
+
+module.exports = connectMongodb
