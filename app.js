9d5c5cc11d1e41d4b1af14fb59e6bf122f09d112
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-08-09 15:33:45 +0100

    Add authentication routes to the application
    - Integrate the auth routes into the main Express application.
    - Prefix authentication routes with `/api/v1/auth` for versioning and organization.

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const bodyParser = require("body-parser");
 const dotenv = require('dotenv')
 dotenv.config()
 const connectMongodb = require("./init/mongodb")
-
+const { authRoute } = require('./routes/')
 
 const app = express();
 
 // Connect database
-
 connectMongodb();
 
 app.use(express.json({limit: "500mb"}));
 app.use(bodyParser.urlencoded({limit: "500mb", extended : true}));
+
+// Routes
+app.use("/api/v1/auth", authRoute)
 
 module.exports = app;
