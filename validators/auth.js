52deaf7f65869f22cd4413ecedceb452dac4a023
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-08-11 13:22:36 +0100

    Implemented express-validator checks for signup form, validating name, email (format and existence), and password (length and existence), returning detailed error messages

---
+++ validators/auth.js
@@ -0,0 +1,22 @@
+const {check} = require("express-validator");
+
+
+const signupValidator = [
+    check("name").notEmpty().withMessage("Name is required"),
+
+
+    check("email")
+            .isEmail()
+            .withMessage("Invalid email")
+            .notEmpty()
+            .withMessage("Email is required"),
+
+     check('password')
+            .isLength({min: 6})
+            .withMessage("Password should be 6 char long")
+            .notEmpty()
+            .withMessage("Password is required")];
+
+
+
+module.exports = { signupValidator }
