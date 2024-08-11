6194dfac017ce6c89a3410f745eadb2670f1dc93
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-08-11 13:22:01 +0100

    Replaced manual input validation with express-validator and added email uniqueness check using User model, throwing 400 error if email already exists

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,24 +2,34 @@ const {User} = require("../models")
 const signup = async (req, res, next) => {
     try {
         const {name, email, password, role} = req.body;
-        if(!name){
-            res.code = 400;
-            throw new Error("Name is required");
-        }
+        // if(!name){
+        //     res.code = 400;
+        //     throw new Error("Name is required");
+        // }
 
-        if(!email){
-            res.code = 400;
-            throw new Error("Email is required");
-        }
+        // if(!email){
+        //     res.code = 400;
+        //     throw new Error("Email is required");
+        // }
 
-        if(!password){
-            res.code = 400;
-            throw new Error("Password is required");
-        }
+        // if(!password){
+        //     res.code = 400;
+        //     throw new Error("Password is required");
+        // }
 
-        if(password.length < 6){
+        // if(password.length < 6){
+        //     res.code = 400;
+        //     throw new Error("Password should be 6 char long.");
+        // }
+
+
+
+        // DO THIS INSTEAD 
+
+        const isEmailExists = User.findOne({email});
+        if(isEmailExists){
             res.code = 400;
-            throw new Error("Password should be 6 char long.");
+            throw new Error("Email already exist") 
         }
         const newUser = new User({name, email, password, role})
         await newUser.save();
