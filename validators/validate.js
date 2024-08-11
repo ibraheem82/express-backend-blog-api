3daf50bfa114f20c1f332f088a5c050fbe43841b
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-08-11 13:21:39 +0100

    Implemented request validation using express-validator, mapping error messages to corresponding fields and returning 400 Bad Request response with detailed error object

---
+++ validators/validate.js
@@ -0,0 +1,30 @@
+const {validationResult} = require("express-validator");
+
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    const mappedErrors = {}
+    // console.log(errors);
+    // next();
+
+
+    // This code checks if there are any validation errors by examining the length of the errors.errors object.
+// If there are no errors (length is 0), the middleware calls next(), allowing the request to proceed to the next middleware or route handler.
+// If there are errors, the code continues to the else block.
+    if(Object.keys(errors.errors).length === 0){
+        next();
+    } else{
+
+        // For each error, it creates a key-value pair in mappedErrors. The key is the field name (error.path) where the error occurred, and the value is the error message (error.msg).
+        errors.errors.map((error) => {
+            mappedErrors[error.path] = error.msg
+        });
+
+
+        // It sends a JSON response containing the mappedErrors object, which provides users with specific information about the validation errors encountered.
+
+        res.status(400).json(mappedErrors)
+    }
+    
+};
+
+module.exports = validate
