54db21bea7cd50e84652f2d9af66785a617beeb4
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-11-11 12:32:40 +0100

    feat: add password comparison utility with bcrypt
    - Created comparePassword function to verify plain text passwords against hashed passwords
    - Uses bcrypt.compare for secure password validation

---
+++ utils/comparePassword.js
@@ -0,0 +1,8 @@
+const bcrypt = require("bcryptjs");
+
+const comparePassword = (password, hashPassword) => {
+    return bcrypt.compare(password, hashPassword);
+}
+
+
+module.exports = comparePassword
