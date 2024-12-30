2a8a9821375dd6170f47194784f3e0660d41d13a
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-12-30 17:34:07 +0100

    feat: add validation middleware for user verification route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const {authController } = require("../controllers/");
-const { signupValidator, signinValidator, emailValidator } = require("../validators/auth");
+const { signupValidator, signinValidator, emailValidator, verifyUserValidator } = require("../validators/auth");
 const validate = require("../validators/validate");
 
 router.post('/signup', signupValidator, validate, authController.signup)
 router.post('/signin', signinValidator, validate,  authController.signin)
 router.post('/send-verification-email', emailValidator, validate, authController.verifyCode)
+router.post('/verify-user', verifyUserValidator, validate, authController.verifyUser)
 module.exports = router;
