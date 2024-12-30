97f3f75fafad4c8ffc2cc532222de294f997d259
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-12-30 17:34:53 +0100

    Add verifyUserValidator array with rules for email and code fields

--- validators/auth.js
+++ validators/auth.js
@@ -40,4 +40,16 @@ const emailValidator = [
               .withMessage("Email is required"),
 ]
 
-module.exports = { signupValidator, signinValidator, emailValidator }
+
+
+
+const verifyUserValidator = [
+       check("email")
+              .isEmail()
+              .withMessage("Invalid email")
+              .notEmpty()
+              .withMessage("Email is required"),
+       check("code").notEmpty().withMessage("Code is required")
+
+]
+module.exports = { signupValidator, signinValidator, emailValidator, verifyUserValidator }
