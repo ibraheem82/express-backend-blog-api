4931f968d407ecdb04d130b1b8e59a70363fb566
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-12-30 17:02:43 +0100

    feat: add environment variable configuration for server and email setup

--- config/kyes.js
+++ config/kyes.js
@@ -1,4 +1,12 @@
-const {PORT, CONNECTION_URL, JWT_SECRET} = process.env;
+const {PORT, 
+    CONNECTION_URL,
+     JWT_SECRET, 
+     GMAIL_USER, 
+     GMAIL_PASSWORD} = process.env;
 
 
-module.exports = {port: PORT, connectionUrl: CONNECTION_URL, jwtSecret: JWT_SECRET}
+module.exports = {port:
+        PORT, connectionUrl:
+      CONNECTION_URL, jwtSecret:
+       JWT_SECRET, gmail_user: GMAIL_USER, 
+       gmail_password: GMAIL_PASSWORD}
