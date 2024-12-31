85c3ba739f2f8b252dfc5d5e81e737110da312c7
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-12-31 10:51:45 +0100

    forgot password code to send mail done...

--- routes/auth.js
+++ routes/auth.js
@@ -8,4 +8,5 @@ router.post('/signup', signupValidator, validate, authController.signup)
 router.post('/signin', signinValidator, validate,  authController.signin)
 router.post('/send-verification-email', emailValidator, validate, authController.verifyCode)
 router.post('/verify-user', verifyUserValidator, validate, authController.verifyUser)
+router.post('/forgot-password-code', emailValidator, validate, authController.forgotPasswordCode)
 module.exports = router;
