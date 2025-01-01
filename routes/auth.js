1c6df531f586a85f8f41ae54907eb049f42d2ef2
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-01-01 16:10:27 +0100

    feat: Implement secure password recovery API route: Adds POST route '/recover-password' Uses recoverPasswordValidator and validate middleware for request validation Calls authController.recoverPassword function to handle password recovery logic

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {authController } = require("../controllers/");
-const { signupValidator, signinValidator, emailValidator, verifyUserValidator } = require("../validators/auth");
+const { signupValidator, signinValidator, emailValidator, verifyUserValidator, recoverPasswordValidator } = require("../validators/auth");
 const validate = require("../validators/validate");
 
 router.post('/signup', signupValidator, validate, authController.signup)
@@ -9,4 +9,5 @@ router.post('/signin', signinValidator, validate,  authController.signin)
 router.post('/send-verification-email', emailValidator, validate, authController.verifyCode)
 router.post('/verify-user', verifyUserValidator, validate, authController.verifyUser)
 router.post('/forgot-password-code', emailValidator, validate, authController.forgotPasswordCode)
+router.post('/recover-password', recoverPasswordValidator, validate, authController.recoverPassword)
 module.exports = router;
