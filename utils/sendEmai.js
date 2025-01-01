a781f5cab1045be7b35f0b50af1e1f0db54e5618
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-01-01 16:13:09 +0100

    feat: Implement email sending functionality for password recovery

---
+++ utils/sendEmai.js
@@ -0,0 +1,35 @@
+const {gmail_user, gmail_password} = require("../config/kyes")
+const nodemailer = require("nodemailer");
+
+
+const sendMail = async ({ emailTo, subject, code, content }) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: gmail_user,
+        pass: gmail_password,
+      },
+    });
+
+    const message = {
+      to: emailTo,
+      subject,
+      html: `
+        <div>
+          <h3>Use this below code to ${content}</h3>
+          <p><strong>Code: </strong> ${code}</p>
+        </div>
+      `,
+    };
+
+    const info = await transporter.sendMail(message);
+    console.log(`Email sent: ${info.response}`);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
+
+module.exports = sendMail;
