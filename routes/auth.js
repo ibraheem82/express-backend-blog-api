7f058becad656e6900529352d03ad0d539bed923
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-01-23 11:12:07 +0100

    isAuth was added if your are not logged in you cant change password...

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const {authController } = require("../controllers/");
-const { signupValidator, signinValidator, emailValidator, verifyUserValidator, recoverPasswordValidator } = require("../validators/auth");
+const { signupValidator, signinValidator, emailValidator, verifyUserValidator, recoverPasswordValidator, changePasswordValidator } = require("../validators/auth");
 const validate = require("../validators/validate");
+const isAuth = require("../middlewares/isAuth")
 
 router.post('/signup', signupValidator, validate, authController.signup)
 router.post('/signin', signinValidator, validate,  authController.signin)
@@ -10,4 +11,5 @@ router.post('/send-verification-email', emailValidator, validate, authController
 router.post('/verify-user', verifyUserValidator, validate, authController.verifyUser)
 router.post('/forgot-password-code', emailValidator, validate, authController.forgotPasswordCode)
 router.post('/recover-password', recoverPasswordValidator, validate, authController.recoverPassword)
+router.put('/change-password', changePasswordValidator, validate,  isAuth, authController.changePassword)
 module.exports = router;
