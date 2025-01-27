1c0ac1722e8cec94033ccd0f8f11ac8fe91e74fd
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-01-27 15:47:21 +0100

    admin middleware was added ...

--- routes/category.js
+++ routes/category.js
@@ -4,9 +4,11 @@ const {categoryController} = require("../controllers");
 const {addCategoryValidator} = require("../validators/category");
 const validate = require("../validators/validate");
 const isAuth = require("../middlewares/isAuth");
+const isAdmin = require('../middlewares/isAdmin');
 
 router.post('/',
      isAuth,
+     isAdmin,
      addCategoryValidator,
       validate, 
       categoryController.addCategory
